d46a812bc0f98ae673b2951966b55c1e41919f47
RickyH <xproxskyx@gmail.com>
2019-06-20 02:38:10 -0400

    Get Data From Parse Server

--- app/src/main/java/com/proxsky/instagramclone/MainActivity.java
+++ app/src/main/java/com/proxsky/instagramclone/MainActivity.java
@@ -3,18 +3,28 @@ package com.proxsky.instagramclone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText name, ps, pp, ks, kp;
+    private TextView txtGetData;
+    private Button btnGetAllData;
+    private String allKickBoxers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,58 @@ public class MainActivity extends AppCompatActivity {
         pp = findViewById(R.id.editText_pp);
         ks = findViewById(R.id.editText_ks);
         kp = findViewById(R.id.editText_kp);
+        txtGetData = findViewById(R.id.txtGetData);
+        btnGetAllData = findViewById(R.id.btnGetAllData);
+
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parseQuery =
+                        ParseQuery.getQuery("KickBoxer");
+                parseQuery.getInBackground("dHSONxrd7L", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object!=null&&e==null)
+                        {
+                            txtGetData.setText(object.get("name")+" "
+                            +object.get("punchPower"));
+                        }
+                    }
+                });
+            }
+        });
+
+        btnGetAllData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                allKickBoxers = "";
+                ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("KickBoxer");
+                queryAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+
+                        if(e==null)
+                        {
+                            if(objects.size()>0)
+                            {
+                                for(ParseObject parseObject : objects)
+                                {
+                                    allKickBoxers = allKickBoxers + parseObject.get("name") + "\n";
+                                }
+                                FancyToast.makeText(MainActivity.this,allKickBoxers,Toast.LENGTH_SHORT,FancyToast.SUCCESS,false).show();
+
+                            }
+                            else
+                            {
+                                FancyToast.makeText(MainActivity.this,"NO SUCCESS",Toast.LENGTH_SHORT,FancyToast.ERROR,false).show();
+                            }
+                        }
+                    }
+                });
+
+            }
+        });
     }
 
     public void helloWorldTapped(View view)
